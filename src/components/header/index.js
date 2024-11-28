a2e621b6b558ec5098812a2c1ca6fd8d28254ef2
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-27 22:29:01 -0300

    need to fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import * as C from "../../styles"
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <C.Container>
+      <C.Header>
+        <C.Title>Controle Financeiro</C.Title>
+      </C.Header>
+    </C.Container>
+  
+  );
+};
 
-export default Header
+export default Header;
